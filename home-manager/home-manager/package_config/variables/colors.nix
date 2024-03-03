30619674dffae93c21002ce70c771dd13c747ddd
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-03 11:46:22 +0100

    [ Sun Mar  3 11:46:22 AM CET 2024 ] home-manager switch

--- home-manager/home-manager/package_config/variables/colors.nix
+++ home-manager/home-manager/package_config/variables/colors.nix
@@ -5,7 +5,8 @@
   hexToRGB = color: { r = lib.fromHex color.r; g = lib.fromHex color.g; b = lib.fromHex color.b; };
 
   opacity = 1;
-  opacity_secondary = 0.90;
+  opacity_secondary = 0.85;
+
   accent = {r = "06"; g = "6c"; b = "fa";};
   background = {r = "25"; g = "25"; b = "25";};
   background_darker = { r = "15"; g = "15"; b = "15"; };
