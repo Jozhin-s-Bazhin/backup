68665eaf2865b01f269f9bd8423500258453347b
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-09 15:40:11 +0100

    [ Sat Mar  9 03:40:11 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -16,7 +16,7 @@
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-	specialArgs = { inherit inputs };
+	specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
