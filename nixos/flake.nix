995b17b497a0a3d610fee327bb001b628f7c9a62
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-09 15:45:42 +0100

    [ Sat Mar  9 03:45:42 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -12,7 +12,7 @@
     hyprland.url = "github:hyprwm/hyprland";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ nixpkgs, home-manager, hyprland, ... }: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
