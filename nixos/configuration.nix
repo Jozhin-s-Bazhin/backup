e7cf7b5b6e202d3302c24f1db99af968bcca8843
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-10 22:24:34 +0100

    [ Sun Mar 10 10:24:34 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,14 +101,14 @@
   #};
 
   # Pipewire
+  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    audio.enable = true;
-
-    alsa = {
-      enable = true;
-      support32Bit = true;
-    };
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
   };
 
   # GTK fix
