414962f325a9f753001e92fff22179ad70af57e2
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-10 18:44:30 +0100

    [ Sun Mar 10 06:44:30 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/hypr/appearance.nix
+++ nixos/home/package_config/hypr/appearance.nix
@@ -7,7 +7,7 @@ in
   wayland.windowManager.hyprland.settings = {
     general = {
       gaps_in = 5;
-      gaps_out = 20;
+      gaps_out = 5;
       border_size = 2;
       "col.active_border" = "rgba(${ colors.nixToHex colors.accent }ff)";
       "col.inactive_border" = "rgba(${ colors.nixToHex colors.background_darker}ff)";
