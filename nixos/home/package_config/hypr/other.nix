9d2a64b14483234dbd989c7df765a849f02a4129
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-10 22:32:36 +0100

    [ Sun Mar 10 10:32:36 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/hypr/other.nix
+++ nixos/home/package_config/hypr/other.nix
@@ -5,5 +5,14 @@
     monitor = [
       ", preferred, auto, 1"
     ];
+
+    windowrulev2 = [
+      # XWaylandvideobridge
+      "opacity 0.0 override 0.0 override,class:^(xwaylandvideobridge)$"
+      "noanim,class:^(xwaylandvideobridge)$"
+      "noinitialfocus,class:^(xwaylandvideobridge)$"
+      "maxsize 1 1,class:^(xwaylandvideobridge)$"
+      "noblur,class:^(xwaylandvideobridge)$"
+    ];
   };
 }
