9b46eacf1dd14af3a423eaa13f1f45cdccc44878
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-11 20:59:19 +0100

    [ Mon Mar 11 08:59:19 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/hypr/keybinds.nix
+++ nixos/home/package_config/hypr/keybinds.nix
@@ -43,7 +43,7 @@ in
 
     binde = [
       ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%+"
-      ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
+      ", XF86AudioLowerVolume, exec, wpctl set-volume -l 1.5 @DEFAULT_AUDIO_SINK@ 5%-"
     ];
   };
 }
