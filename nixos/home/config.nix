bb2f42873c02fe011e11b0ac542722f492bb4195
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-11 22:06:00 +0100

    [ Mon Mar 11 10:06:00 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/config.nix
+++ nixos/home/config.nix
@@ -5,6 +5,18 @@
   home.homeDirectory = "/home/roman";
   home.stateVersion = "23.11"; # Do not change
 
+  # Create systemd service to lock session before sleep
+  systemd.user.services.lock-before-sleep = {
+    description = "Lock session before sleep";
+    partOf = [ "sleep.target" ];
+    before = [ "sleep.target" ];
+    wantedBy = [ "sleep.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.systemd}/bin/loginctl lock-session & sleep 3";
+    };
+  };
+
   # Enable XDG desktop portal
   xdg.portal = {
     enable = true;
