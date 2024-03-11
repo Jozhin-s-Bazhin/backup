4977ded6cfbda4d70bdc298046cdfcc050f72d7c
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-11 22:08:28 +0100

    [ Mon Mar 11 10:08:28 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/config.nix
+++ nixos/home/config.nix
@@ -6,11 +6,10 @@
   home.stateVersion = "23.11"; # Do not change
 
   # Create systemd service to lock session before sleep
-  systemd.user.services.lock-before-sleep = {
-    description = "Lock session before sleep";
-    partOf = [ "sleep.target" ];
-    before = [ "sleep.target" ];
+  systemd.user.services.lockScreenBeforeSleep = {
+    description = "Lock screen before sleep";
     wantedBy = [ "sleep.target" ];
+    before = [ "sleep.target" ]; # Ensure this is a simple list of strings
     serviceConfig = {
       Type = "oneshot";
       ExecStart = "${pkgs.systemd}/bin/loginctl lock-session & sleep 3";
