69f994c5581fafb17b81f0a9359ed3f9d562646b
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-15 20:11:50 +0100

    [ Fri Mar 15 08:11:50 PM CET 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/other.nix
+++ nixos/home/package_config/other.nix
@@ -32,8 +32,9 @@ in
   };
 
   # VSCode
-  programs.vscodium = {
+  programs.vscode = {
     enable = true;
+    package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       asvetliakov.vscode-neovim
       ms-python.python
