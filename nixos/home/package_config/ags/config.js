4c935101c3d70ef86f2b57a8af63204dddf49c6b
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-31 13:34:18 +0200

    [ Sun Mar 31 01:34:18 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/config.js
+++ nixos/home/package_config/ags/config.js
@@ -126,7 +126,7 @@ function BatteryLabel() {
     //const icon = battery.bind("percent").as(p =>
     //    `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
 
-    return Widget.Label(`${battery}%`)
+    return Widget.Label(`${battery.bind("percent")}%`)
 
     //return Widget.Box({
     //    class_name: "battery",
