3276cc941bd0de2fd50052148d4f6d7f515ea29e
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-02 11:54:27 +0200

    [ Tue Apr  2 11:54:27 AM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/ags.nix
+++ nixos/home/package_config/ags/ags.nix
@@ -30,11 +30,13 @@ in
   home.file.".config/ags/config.js".source = ./config.js;
   home.file.".config/ags/bar/".source = ./bar;
   home.file.".config/ags/colors.css".text = ''
+    * {
     --opacity: ${toString colors.opacity};
     --opacity-secondary: ${toString colors.opacity_secondary};
 
     --accent: ${colors.nixToHex colors.accent};
     --background: ${colors.nixToHex colors.background};
     --background-darker: ${colors.nixToHex colors.background_darker};
+    }
   '';
 }
