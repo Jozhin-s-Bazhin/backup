c8c8097545525bdc9b71b2a3f42985027cfb8092
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-05 16:19:02 +0200

    [ Fri Apr  5 04:19:02 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/hypr/other.nix
+++ nixos/home/package_config/hypr/other.nix
@@ -4,8 +4,7 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       ", preferred, auto, 1"
-      #"DP-1, preferred, 1920x1080, 1"
-      "desc:Huawei Technologies Co. Inc. ZQE-CAA 0xC080F622, preferred, 2680x1440, 1"
+      "desc:Huawei Technologies Co. Inc. ZQE-CAA 0xC080F622, preferred, 2680x-1440, 1"
     ];
 
     windowrulev2 = [
