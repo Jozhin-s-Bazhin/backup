a12f8ce4c0b18732139d4bbfeab433137bd7e9fa
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-07 11:41:48 +0200

    [ Sun Apr  7 11:41:48 AM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/scripts/pypr/better_workspaces.py
+++ nixos/home/scripts/pypr/better_workspaces.py
@@ -1,12 +1,29 @@
 from pyprland.plugins.interface import Plugin
 
 
-FIRST_WORKSPACE = 2147483647 / 2
+MAX_WORKSPACE_ID = 2147483647
+ROOT_WORKSPACE_ID = MAX_WORKSPACE_ID / 2
 
 async def get_workspaces(self):
     workspaces = [ workspace["id"] for workspace in (await self.hyprctlJSON("workspaces")) if workspace["id"] > 0 ]
     workspaces.sort()
     return workspaces
+
+def insert_last_workspace(workspaceid):
+    target = (MAX_WORKSPACE_ID - workspaceid) // 2
+    print(target)
+    return target
+ 
+async def get_target_workspace(self, workspaceid):
+    workspaceid = int(workspaceid)
+    workspaces = await get_workspaces(self)
+    
+    if workspaceid > len(workspaces):
+        target = insert_last_workspace(workspaces[-1])
+    else:
+        target = workspaces[workspaceid - 1]
+        
+    return target
 
 async def move_workspaces_to_focused_mon(self, event_data):
     """Takes in a value the hyprland socket gives when changing monitors and moves all inactive windows to that monitor"""
@@ -22,17 +39,6 @@ async def move_workspaces_to_focused_mon(self, event_data):
             
     if batch:
         await self.hyprctl(batch)
- 
-async def get_target_workspace(self, workspaceid):
-    workspaceid = int(workspaceid)
-    workspaces = await get_workspaces(self)
-    
-    if workspaceid > len(workspaces):
-        target = workspaces[-1] + 1  # Gap
-    else:
-        target = workspaces[workspaceid - 1]
-        
-    return target
 
 class Extension(Plugin):
     """A plugin for better workspaces, similar to Gnome's workspace behaviour, but also with a function to insert workspaces"""
@@ -47,15 +53,3 @@ class Extension(Plugin):
     async def run_movetoworkspace(self, workspaceid):
         target = await get_target_workspace(self, workspaceid)
         await self.hyprctl(f"movetoworkspace {target}")
-        
-    #async def run_insertworkspace(self, workspace):
-    #    target = await get_target_inserted_workspace(self, workspace)
-    #    await self.hyprctl(f"workspace {target}")
-
-    #async def run_movetoinsertedworkspace(self, workspace):
-    #    target = await get_target_inserted_workspace(self, workspace)
-    #    await self.hyprctl(f"movetoworkspace {target}")
-    
-    async def event_workspace(self, event_data):
-        workspaces = await get_workspaces(self)
-        if workspaces[-1] == workspaces[-2] + 1:
