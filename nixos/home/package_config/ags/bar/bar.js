ebca72d37283d9cb5aa7d1648916764cdb7a32d1
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-07 16:18:39 +0200

    [ Sun Apr  7 04:18:39 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/bar/bar.js
+++ nixos/home/package_config/ags/bar/bar.js
@@ -16,11 +16,11 @@ const date = Variable("", {
 function Workspaces() {
     const activeId = hyprland.active.workspace.bind("id")
     const workspaces = hyprland.bind("workspaces")
-        .as(ws => ws.map(({ id }) => Widget.Button({
+        .as(ws => ws.map(({ id }) => id > 0 ? Widget.Button({
             on_clicked: () => hyprland.messageAsync(`dispatch workspace ${id}`),
             child: Widget.Label(id >= 0 ? `${id}`: ""),
             class_name: activeId.as(i => `${i === id ? "focused" : ""}`),
-        })))
+        }) : "" ))
 
     return Widget.Box({
         class_name: "workspaces",
