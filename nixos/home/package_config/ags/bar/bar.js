104faeeb5b403858722340a9688b4ba9b9e950ea
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-08 13:19:45 +0200

    [ Mon Apr  8 01:19:44 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/bar/bar.js
+++ nixos/home/package_config/ags/bar/bar.js
@@ -102,7 +102,7 @@ function Volume() {
 
 function BatteryLabel() {
     const icon = battery.bind("percent").as(p =>
-        `battery-level-${Math.floor(p / 10) * 10}-symbolic`)
+        `battery-level-${Math.floor(p / 10) * 10}${battery.bind("charging").as(c ? "-charging" : "")}-symbolic`)
 
     return Widget.Box({
         class_name: "battery",
