1a480e22c83500a6f7def830597ec6263d2acd3d
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-08 17:34:00 +0200

    [ Mon Apr  8 05:34:00 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -123,7 +123,6 @@
 
   # Kde connect
   services.dbus.enable = true;
-  programs.kdeconnect.enable = true;
   networking.firewall.allowedTCPPorts = [ 1714 1715 ];
   networking.firewall.allowedUDPPorts = [ 1714 1715 1716 ];
 
