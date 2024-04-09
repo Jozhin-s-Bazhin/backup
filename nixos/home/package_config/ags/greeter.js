41e22d4895bc0359cfab7af4b5fa9255eb25ca9b
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-09 18:06:24 +0200

    [ Tue Apr  9 06:06:24 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/greeter.js
+++ nixos/home/package_config/ags/greeter.js
@@ -1,11 +1,13 @@
 const greetd = await Service.import('greetd');
 
 const name = Widget.Entry({
+    css: "color: black; background-color: white; border-radius: 10px;"
     placeholder_text: 'Username',
     on_accept: () => password.grab_focus(),
 })
 
 const password = Widget.Entry({
+    css: "color: black; background-color: white; border-radius: 10px;"
     placeholder_text: 'Password',
     visibility: false,
     on_accept: () => {
@@ -20,6 +22,7 @@ const win = Widget.Window({
     css: 'background-image: url("/etc/nixos/home/package_config/wallpaper_blurred.png");',
     anchor: ['top', 'left', 'right', 'bottom'],
     child: Widget.Box({
+	    css: "background-color: blue",
         vertical: true,
         hpack: 'center',
         vpack: 'center',
