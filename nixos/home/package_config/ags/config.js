d10f21862c79808cc5770799b8b4eb204186403d
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-10 20:34:07 +0200

    [ Wed Apr 10 08:34:07 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/config.js
+++ nixos/home/package_config/ags/config.js
@@ -4,7 +4,7 @@ import { NotificationPopups } from "./notifications/NotificationPopups.js";
 App.config({
     style: App.configDir + "style.css",
     windows: [
-        Bar(0)
+        Bar(0),
 	NotificationPopups()
     ]
 })
