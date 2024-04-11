4b262b469c5ecde6f2b9ee1cbc43c7802167d9ac
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-11 20:23:44 +0200

    [ Thu Apr 11 08:23:44 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/greeter.js
+++ nixos/home/package_config/ags/greeter.js
@@ -1,10 +1,12 @@
 const greetd = await Service.import('greetd');
 
-/*const name = Widget.Entry({
+const users = Utils.exec("getent passwd | awk -F: '$3 >= 1000 && $3 < 60000 && !/^nixbld/ {print $1}' | grep -v '^nixbld'")
+
+const name = Widget.Entry({
     css: "color: black; background-color: white; border-radius: 10px;",
     placeholder_text: 'Username',
     on_accept: () => password.grab_focus(),
-})*/
+})
 
 const password = Widget.Entry({
     css: "color: black; background-color: white; border-radius: 10px;",
@@ -12,7 +14,7 @@ const password = Widget.Entry({
     visibility: false,
     on_accept: () => {
         greetd.login("roman" || '', password.text || '', 'Hyprland')
-            .catch(() => password.text = "", password.grab_focus())
+            .catch(() => password.text = "", name.grab_focus())
     },
 })
 
@@ -29,13 +31,13 @@ const win = Widget.Window({
     css: 'background-image: url("/etc/nixos/home/package_config/wallpaper_blurred.png");',
     anchor: ['top', 'left', 'right', 'bottom'],
     child: Widget.Box({
-        vertical: false,
+        vertical: true,
         hpack: 'center',
         vpack: 'center',
         hexpand: true,
         vexpand: true,
         children: [
-            stack,
+            name,
             password,
         ],
     }),
