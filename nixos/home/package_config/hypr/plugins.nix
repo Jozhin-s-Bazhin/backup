bf7e4220328580ea418229640671fb5d17771320
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-12 23:22:42 +0200

    [ Fri Apr 12 11:22:42 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/hypr/plugins.nix
+++ nixos/home/package_config/hypr/plugins.nix
@@ -1,6 +1,7 @@
 { config, pkgs, inputs, ... }:
 {
   wayland.windowManager.hyprland.plugins = [
-    inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
+    #inputs.Hyprspace.packages.${pkgs.system}.Hyprspace
+    /nix/store/cb7jyqfw13fakgj2q9w25dzadgc6033a-Hyprspace-0.1/lib/libHyprspace.so
   ];
 }
