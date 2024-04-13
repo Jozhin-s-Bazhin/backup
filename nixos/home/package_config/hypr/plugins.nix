f8aa16773b5da4b9d647e9def8a20b8c1d93b541
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-14 00:21:02 +0200

    [ Sun Apr 14 12:21:02 AM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/hypr/plugins.nix
+++ nixos/home/package_config/hypr/plugins.nix
@@ -8,7 +8,7 @@
   wayland.windowManager.hyprland.settings = {
     plugin = {
       overview = {
-        panelColor = "rgba(b3ff1a00)";
+        panelColor = "rgba(b3ff1aff)";
 	panelHeight = "150";
 	showEmptyWorkspace = false;
       };
