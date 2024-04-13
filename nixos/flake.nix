21e06d4b606d5671677f3ba001f33040f1765209
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-13 11:43:13 +0200

    [ Sat Apr 13 11:43:13 AM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,10 +10,10 @@
     ags.url = "github:Aylur/ags";
     hyprlock.url = "github:hyprwm/hyprlock";
     hyprland.url = "github:hyprwm/Hyprland/";
-    Hyprspace = {
-      url = github:KZDKM/Hyprspace;
-      inputs.hyprland.follows = "hyprland";
-    };
+    #Hyprspace = {
+    #  url = github:KZDKM/Hyprspace;
+    #  inputs.hyprland.follows = "hyprland";
+    #};
     #hy3 = {
     #  url = "github:outfoxxed/hy3";
     #  inputs.hyprland.follows = "hyprland";
@@ -38,8 +38,8 @@
             home-manager.users.roman = import ./home/home.nix;
 	          home-manager.extraSpecialArgs = { inherit inputs; };
           }
-#	        hyprland.nixosModules.default
-#         {programs.hyprland.enable = true;}
+	  hyprland.nixosModules.default
+          {programs.hyprland.enable = true;}
         ];
       };
     };
