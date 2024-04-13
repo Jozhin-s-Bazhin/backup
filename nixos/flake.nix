0c2c6c23428f56ea654541b356c1c2eef42da08c
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-13 22:45:54 +0200

    [ Sat Apr 13 10:45:54 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -41,5 +41,13 @@
         ];
       };
     };
+        homeConfigurations."roman@nixos" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      modules = [
+        hyprland.homeManagerModules.default
+        {wayland.windowManager.hyprland.enable = true;}
+      ];
+    };
   };
 }
