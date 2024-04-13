11600d6c164b380f1cbe757e79627434e728019b
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-13 22:34:43 +0200

    [ Sat Apr 13 10:34:43 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -36,7 +36,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.roman = import ./home/home.nix;
-	          home-manager.extraSpecialArgs = { inherit inputs; };
+	    home-manager.extraSpecialArgs = { inherit inputs; };
           }
         ];
       };
