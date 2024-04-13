bd87d24fe6799aa8498f4b398a338e0582d1d559
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-13 23:15:10 +0200

    [ Sat Apr 13 11:15:10 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/greeter.js
+++ nixos/home/package_config/ags/greeter.js
@@ -1,34 +1,23 @@
 const greetd = await Service.import('greetd');
 
-const users = Utils.exec("getent passwd | awk -F: '$3 >= 1000 && $3 < 60000 && !/^nixbld/ {print $1}' | grep -v '^nixbld'")
-
 const name = Widget.Entry({
-    css: "color: black; background-color: white; border-radius: 10px;",
     placeholder_text: 'Username',
     on_accept: () => password.grab_focus(),
 })
 
 const password = Widget.Entry({
-    css: "color: black; background-color: white; border-radius: 10px;",
     placeholder_text: 'Password',
     visibility: false,
     on_accept: () => {
-        greetd.login("roman" || '', password.text || '', 'Hyprland')
-            .catch(() => password.text = "", name.grab_focus())
+        greetd.login(name.text || '', password.text || '', 'Hyprland')
+            .catch(err => response.label = JSON.stringify(err))
     },
 })
 
-const stack = Widget.ListBox({
-    setup(self) {
-        self.add(Widget.Button({
-            child: Widget.Label("roman"),
-            onClicked: () => password.grab_focus(),
-        }))
-    },
-})
+const response = Widget.Label()
 
 const win = Widget.Window({
-    css: 'background-image: url("/etc/nixos/home/package_config/wallpaper_blurred.png");',
+    css: 'background-color: transparent;',
     anchor: ['top', 'left', 'right', 'bottom'],
     child: Widget.Box({
         vertical: true,
@@ -39,53 +28,8 @@ const win = Widget.Window({
         children: [
             name,
             password,
-        ],const greetd = await Service.import('greetd');
-
-        const users = Utils.exec("getent passwd | awk -F: '$3 >= 1000 && $3 < 60000 && !/^nixbld/ {print $1}' | grep -v '^nixbld'")
-        
-        const name = Widget.Entry({
-            css: "color: black; background-color: white; border-radius: 10px;",
-            placeholder_text: 'Username',
-            on_accept: () => password.grab_focus(),
-        })
-        
-        const password = Widget.Entry({
-            css: "color: black; background-color: white; border-radius: 10px;",
-            placeholder_text: 'Password',
-            visibility: false,
-            on_accept: () => {
-                greetd.login("roman" || '', password.text || '', 'Hyprland')
-                    .catch(() => password.text = "", name.grab_focus())
-            },
-        })
-        
-        const stack = Widget.ListBox({
-            setup(self) {
-                self.add(Widget.Button({
-                    child: Widget.Label("roman"),
-                    onClicked: () => password.grab_focus(),
-                }))
-            },
-        })
-        
-        const win = Widget.Window({
-            css: 'background-image: url("/etc/nixos/home/package_config/wallpaper_blurred.png");',
-            anchor: ['top', 'left', 'right', 'bottom'],
-            child: Widget.Box({
-                vertical: true,
-                hpack: 'center',
-                vpack: 'center',
-                hexpand: true,
-                vexpand: true,
-                children: [
-                    name,
-                    password,
-                ],
-            }),
-        })
-        
-        App.config({ windows: [win] })
-        
+            response,
+        ],
     }),
 })
 
