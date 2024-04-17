853085cce971b66642a7f0cc0598935ab8dec1e6
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-04-17 11:36:24 +0200

    [ Wed Apr 17 11:36:24 AM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/ags/greeter.js
+++ nixos/home/package_config/ags/greeter.js
@@ -1,67 +1,36 @@
 const greetd = await Service.import('greetd');
 
-/*async function getUsernames() {
-    const output = await Utils.exec("getent passwd | awk -F: '$3 >= 1000 && $3 < 60000 && !/^nixbld/ {print $1}' | grep -v '^nixbld'");
-    return output.split('\n').filter(user => user.trim() !== '');
-}*/
-
-//const users = await getUsernames();
-const users = [ "roman" ];
-
-const userStack = Widget.ListBox({
-    css: "background-color: red;",
-    setup(self) {
-        users.forEach(user => {
-            self.add(Widget.Button({
-                child: Widget.Label(user),
-                onClicked: () => {
-                    username = "roman"
-                    password.grab_focus();
-                },
-            }))
-        });
-    },
-});
-
-/*const name = Widget.Entry({
-    css: "color: black; background-color: white; border-radius: 10px;",
+const name = Widget.Entry({
     placeholder_text: 'Username',
     on_accept: () => password.grab_focus(),
-    editable: false, // Making username field non-editable as it's selected from list
-});*/
+})
 
 const password = Widget.Entry({
-    css: "color: black; background-color: white; border-radius: 10px;",
     placeholder_text: 'Password',
     visibility: false,
     on_accept: () => {
-        greetd.login('roman' || '', password.text || '', 'Hyprland')
-            .catch(() => {
-                password.grab_focus();
-            });
+        greetd.login(name.text || '', password.text || '', 'Hyprland')
+            .catch(err => response.label = JSON.stringify(err))
     },
-});
+})
 
-const loginBox = Widget.Box({
-    vertical: true,
-    hpack: 'center',
-    vpack: 'center',
-    hexpand: true,
-    vexpand: true,
-    children: [
-        //name,
-        password,
-    ],
-});
-
-const mainArea = Widget.Box({
-    children: [ userStack, loginBox],
-});
+const response = Widget.Label()
 
 const win = Widget.Window({
-    css: 'background-image: url("/etc/nixos/home/package_config/wallpaper_blurred.png");',
+    css: 'background-color: transparent;',
     anchor: ['top', 'left', 'right', 'bottom'],
-    child: mainArea,
-});
+    child: Widget.Box({
+        vertical: true,
+        hpack: 'center',
+        vpack: 'center',
+        hexpand: true,
+        vexpand: true,
+        children: [
+            name,
+            password,
+            response,
+        ],
+    }),
+})
 
-App.config({ windows: [win] });
+App.config({ windows: [win] })
