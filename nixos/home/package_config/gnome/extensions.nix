71fb2f6193e68c9e2b86521fe87588d02f8fe750
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-05-14 19:51:27 +0200

    [ Tue May 14 07:51:27 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/gnome/extensions.nix
+++ nixos/home/package_config/gnome/extensions.nix
@@ -4,7 +4,7 @@
     "org/gnome/shell" = {
       disable-user-extensions = false;
       enabled-extensions = [
-        "forge@forge-ext.forge.github.com"
+        "forge"
 	"user-theme@gnome-shell-extensions.gcampax.github.com"
 	"blur-my-shell@aunetx"
 	"switch-workspaces-on-active-monitor@gnome-shell-extension-simulate-switching-workspaces-on-active-monitor.micheledaros.github.com"
