a28cbe9f37e8a5f9e4082ca40d1f3550c5c5dd8c
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-05-22 16:19:51 +0200

    [ Wed May 22 04:19:51 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -149,7 +149,7 @@
   # Pipewire
   #sound.enable = true;
   security.rtkit.enable = true;
-  #hardware.pulseaudio.enable = false;
+  hardware.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
