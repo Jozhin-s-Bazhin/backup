04dea0d9d48350a1936a521fb1719f32942e5c7e
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-06-08 15:27:10 +0200

    [ Sat Jun  8 03:27:10 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/config.nix
+++ nixos/home/config.nix
@@ -31,6 +31,7 @@
 
   # WLUMA config
   xdg.configFile."wluma/config.toml".text = ''
+    [[als.iio]]
     path = "/sys/bus/iio/devices"
     thresholds = { 0 = "night", 20 = "dark", 80 = "dim", 250 = "normal", 500 = "bright", 800 = "outdoors" }
 
