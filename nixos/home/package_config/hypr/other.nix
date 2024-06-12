fb9aa1d1a996665d7ae095dc686a2dc800bb29d1
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-06-12 15:27:32 +0200

    [ Wed Jun 12 03:27:32 PM CEST 2024 ] nixos-rebuild switch --flake /etc/nixos#nixos

--- nixos/home/package_config/hypr/other.nix
+++ nixos/home/package_config/hypr/other.nix
@@ -20,6 +20,9 @@
       "noinitialfocus,class:^(xwaylandvideobridge)$"
       "maxsize 1 1,class:^(xwaylandvideobridge)$"
       "noblur,class:^(xwaylandvideobridge)$"
+
+      # LibreOffice
+      "nofullscreenrequest,class:$libreoffice-*"
     ];
 
     workspace = [
